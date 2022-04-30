7d83362bcb683bff2e03eb421722620c8ca19667
meki <mekkdi@gmail.com>
2022-04-30 11:27:41 -0700

    My new changes

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 const choices = ["rock", "paper", "scissors"]
-let computerSelection;
 
 function computerPlay () {   
   const pickOne = choices[Math.floor(Math.random() * choices.length)]; 
   computerSelection = pickOne;
   return computerSelection;
 }
-console.log(computerPlay())
+function playRound (playerSelection, computerSelection)  {
 
-console.log(computerSelection)
-
-function playRound ()  {
-  
-
-  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+  if (playerSelection === computerSelection) {
     return "It's a tie!";
   }
 
-  if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
     return "You win! Rock beats Scissors";
   }
 
-  if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-    return "You win! Scissors beats Paper";
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper"; 
   }
 
-  if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+  if (playerSelection === "paper" && computerSelection === "rock") {
     return "You win! Paper beats Rock";
   }
 
-  if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+  if (playerSelection === "scissors" && computerSelection === "rock") {
     return "You lose! Rock beats Scissors";
   }
 
-  if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+  if (playerSelection === "paper" && computerSelection === "scissors") {
     return "You lose! Scissors beats Paper";
   }
 
-  if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+  if(playerSelection === "rock" && computerSelection === "paper") {
     return "You lose! Paper beats Rock";
   }
 };
-const playerSelection = "rOcK"
-console.log (playerSelection)
-console.log(playRound(playerSelection, computerSelection));
 
+function game() {
+  let computerScore=0;
+  let playerScore=0;
+  let message;
+
+  for (let i=0; i<5; i++) {
+      let playerSelection = prompt ("?")
+    console.log(playerSelection);
+      let computerSelection = computerPlay();
+    console.log(computerSelection);
+      let roundResult = playRound(playerSelection,computerSelection);
+console.log(playRound(playerSelection, computerSelection))
+      if(roundResult.includes("win")) {
+        playerScore++;
+      } else if (roundResult.includes("lose")) {
+        computerScore++
+      }
+  }
+message = (playerScore > computerScore) ? "you win!":
+          (playerScore < computerScore) ? "you lose!":
+          "draw!";
+
+return `${message}`;
+}
+
+console.log(game());
